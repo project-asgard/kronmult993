e6031402d406bd967ae89ae7888869a11b16c891
Nestor Demeure <nestordemeure@gmail.com>
2021-04-30 14:40:46 -0700

    added stride

--- kronmult/kronmult.hpp
+++ kronmult/kronmult.hpp
@@ -6,7 +6,7 @@
  * Computes Y = X^T * M^T
  *      <=> Y[i,j] = X[k,i] * M[j,k]
  *
- * M is a `size_M` by `size_M` matrix
+ * M is a `size_M` by `size_M` matrix of stride `matrix_stride`
  * X is a `size_M` by `nb_col_X` matrix
  * Y is a `nb_col_X` by `size_M` matrix
  * workspace is a `size_M`*`nb_col_X` vector that can be sued to store intermediate values if needed
@@ -18,7 +18,7 @@
  */
 template<typename T>
 GLOBAL_FUNCTION void multiply_transpose(const T X[], const unsigned int nb_col_X,
-                                        const T M[], const unsigned int size_M,
+                                        const T M[], const unsigned int size_M, const unsigned int stride_M,
                                         T Y[], T workspace[])
 {
     #define colmajor(row, col, nb_rows) ((row) + (col) * (nb_rows))
@@ -34,7 +34,7 @@ GLOBAL_FUNCTION void multiply_transpose(const T X[], const unsigned int nb_col_X
             Y[colmajor(colX,rowM,nb_col_X)] = 0.;
             for(unsigned int k=0; k < size_M; k++)
             {
-                Y[colmajor(colX,rowM,nb_col_X)] += X[colmajor(k,colX,size_M)] * M[colmajor(rowM,k,size_M)];
+                Y[colmajor(colX,rowM,nb_col_X)] += X[colmajor(k,colX,size_M)] * M[colmajor(rowM,k,stride_M)];
             }
         }
     }
@@ -45,7 +45,7 @@ GLOBAL_FUNCTION void multiply_transpose(const T X[], const unsigned int nb_col_X
 /*
  * Computes output = kron(matrix_list) * input
  *
- * `matrix_list` is an array containing pointers to `matrix_number` square matrices of size `matrix_size` by `matrix_size`
+ * `matrix_list` is an array containing pointers to `matrix_number` square matrices of size `matrix_size` by `matrix_size` and stride `matrix_stride`
  * `input` is a `matrix_size`^`matrix_number` elements vector
  * `output` is a `matrix_size`^`matrix_number` elements vector, where the output will be stored
  * `workspace` is a `matrix_size`^`matrix_number` elements vector, to be used as workspace
@@ -53,7 +53,7 @@ GLOBAL_FUNCTION void multiply_transpose(const T X[], const unsigned int nb_col_X
  * WARNING: `input` and `workspace` will be used as temporary workspaces and thus modified
  */
 template<typename T>
-GLOBAL_FUNCTION void kronmult(const unsigned int matrix_number, const unsigned int matrix_size, T const* const matrix_list[],
+GLOBAL_FUNCTION void kronmult(const unsigned int matrix_number, const unsigned int matrix_size, T const* const matrix_list[], const unsigned int matrix_stride,
                               T input[],
                               T output[], T workspace[])
 {
@@ -65,7 +65,7 @@ GLOBAL_FUNCTION void kronmult(const unsigned int matrix_number, const unsigned i
     {
         // takes `matrix` into account and put the result in `workspace` (use `output` as a workspace if needed)
         const T matrix[] = matrix_list[i];
-        multiply_transpose<T>(input, nb_col_input, matrix, matrix_size, workspace, output);
+        multiply_transpose<T>(input, nb_col_input, matrix, matrix_size, matrix_stride, workspace, output);
         // swap `input` and `workspace` such that `input` contains once again the input
         std::swap(input, workspace);
     }
@@ -78,7 +78,7 @@ GLOBAL_FUNCTION void kronmult(const unsigned int matrix_number, const unsigned i
 /*
  * Computes output[K] = kron(matrix_list[K]) * input[K] for 0 <= k < batchCount
  *
- * `matrix_list_batched` is an array of `nb_batch`*`matrix_number` pointers to square matrices of size `matrix_size` by `matrix_size`
+ * `matrix_list_batched` is an array of `nb_batch`*`matrix_number` pointers to square matrices of size `matrix_size` by `matrix_size` and stride `matrix_stride`
  * `input_batched` is an array of `nb_batch` vectors of size `matrix_size`^`matrix_number`
  * `output_batched` is an array of `nb_batch` vectors of size `matrix_size`^`matrix_number`, where the outputs will be stored
  * `workspace` is an array of `nb_batch` vectors of size `matrix_size`^`matrix_number`, to be used as workspaces
@@ -86,8 +86,7 @@ GLOBAL_FUNCTION void kronmult(const unsigned int matrix_number, const unsigned i
  * WARNING: `input_batched` and `workspace_batched` will be used as temporary workspaces and thus modified
  */
 template<typename T>
-GLOBAL_FUNCTION void kronmult_batched(const unsigned int matrix_number, const unsigned int matrix_size, T const* const matrix_list_batched[],
-                                      const unsigned int lda, // TODO what does lda represents?
+GLOBAL_FUNCTION void kronmult_batched(const unsigned int matrix_number, const unsigned int matrix_size, T const* const matrix_list_batched[], const unsigned int matrix_stride,
                                       T* input_batched[],
                                       T* output_batched[], T* workspace_batched[],
                                       const unsigned int nb_batch)
@@ -100,6 +99,6 @@ GLOBAL_FUNCTION void kronmult_batched(const unsigned int matrix_number, const un
         const T* input = input_batched[i];
         const T* output = output_batched[i];
         const T* workspace = workspace_batched[i];
-        kronmult<T>(matrix_number, matrix_size, matrix_list, input, output, workspace);
+        kronmult<T>(matrix_number, matrix_size, matrix_list, matrix_stride, input, output, workspace);
     }
 }
