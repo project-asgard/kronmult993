07dd05e9c87660be477f7ed8620a8aecf956483f
Hugo Brunie <hbrunie0@gmail.com>
2021-05-09 22:14:29 +0200

    Adding origin algo.

---
+++ algo-origin/kronmult/pbatched/pbatched.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "kronmult1_pbatched.hpp"
+#include "kronmult2_pbatched.hpp"
+#include "kronmult3_pbatched.hpp"
+#include "kronmult4_pbatched.hpp"
+#include "kronmult5_pbatched.hpp"
+#include "kronmult6_pbatched.hpp"
---
+++ algo-origin/kronmult/xbatched/xbatched.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "kronmult1_xbatched.hpp"
+#include "kronmult2_xbatched.hpp"
+#include "kronmult3_xbatched.hpp"
+#include "kronmult4_xbatched.hpp"
+#include "kronmult5_xbatched.hpp"
+#include "kronmult6_xbatched.hpp"
