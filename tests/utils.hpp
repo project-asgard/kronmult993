925e7910c06915ae5744eb5128b0c18c44b3ad72
Hugo Brunie <hugo.brunie@inria.fr>
2021-05-13 21:12:12 +0200

    Push to cori...

--- tests/utils.hpp
+++ tests/utils.hpp
@@ -24,6 +24,11 @@ void init_array_pointer_kernel(T ** X_p, T* X,
 template <typename T>
 __host__ __device__ void init_array_pointer(T ** X_p, T* X,
                                             size_t outer_size, size_t inner_size);
+
+template<typename T>
+void initialize_pointers_device( T *** matrix_list_batched_pp, T *** input_batched_pp, T *** output_batched_pp,
+                                 T *** workspace_batched_pp, size_t batch_count, size_t matrix_count, size_t dimensions,
+                                 size_t size_input, size_t matrix_size, size_t matrix_stride, size_t grid_level);
 #else
 template <typename T>
 void init_array_pointer(T ** X_p, T* X,
