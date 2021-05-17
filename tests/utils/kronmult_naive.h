4a1b60b205764da77af401a91621ead3bbce916e
Nestor Demeure <nestordemeure@gmail.com>
2021-05-17 11:31:51 -0700

    added naive implementation of kronmult for tests

---
+++ tests/utils/kronmult_naive.h
@@ -0,0 +1,112 @@
+#pragma once
+
+/*
+ * computes number^power for integers
+ * does not care about performances
+ * does not use std::pow as it does an implicit float conversion that could lead to rounding errors for high numbers
+ */
+int pow_int(const int number, const int power)
+{
+    if(power == 0) return 1;
+    return number * pow_int(number, power-1);
+}
+
+/*
+ * converts row and col indices into a single index for a matrix store in col-major
+ * `stride` is usually the number of rows of the matrix
+ */
+constexpr int colmajor(const int row, const int col, const int stride)
+{
+    return row + col*stride;
+}
+
+/*
+ * naive implementation of a matrix vector product
+ * we suppose that the matrix is square, stored in col-major and of stride one
+ * the result is *added* to output
+ */
+template<typename T>
+void matrix_vector_product(const T matrix[], const int size
+                           const T vector[], T output[])
+{
+    for(int row = 0; row < size; row++)
+    {
+        for(int col = 0; col < size; col++)
+        {
+            output[row] += matrix[colmajor(row,col,size)] * vector[k];
+        }
+    }
+}
+
+/*
+ * does the kronecker product of two square col-major matrices
+ * the output is a square col-major matrix of stride its number of columns
+ */
+template<typename T>
+void kronecker_product(const T matrix1[], const int size1, const int stride1,
+                       const T matrix2[], const int size2, const int stride2,
+                       T output[])
+{
+    const int stride_out = size1*size2;
+    for(int row1 = 0; row1 < size1; row1++)
+    {
+        for(int row2 = 0; row2 < size2; row2++)
+        {
+            for(int col1 = 0; col1 < size1; col1++)
+            {
+                for(int col2 = 0; col2 < size2; col2++)
+                {
+                    const int row_out = row1*size2 + row2;
+                    const int col_out = col1*size1 + col2
+                    output[colmajor(row_out,col_out,stride_out)] = matrix1[colmajor(row1,col1,stride1)] * matrix2[colmajor(row2,col2,stride2)];
+                }
+            }
+        }
+    }
+}
+
+/*
+ * naive implementation of kronmult
+ */
+template<typename T>
+void kronmult_naive(const int matrix_count, const int matrix_size, T const * const matrix_list[], const int matrix_stride, T input[], T output[])
+{
+    // computes the kronnecker product
+    //T* kronmat = new T[size_input*size_input];
+    //T* kronmat_workspace = new T[size_input*size_input];
+    T* kronmat = matrix_list[0];
+    int size_kron = matrix_size;
+    int stride_kron = matrix_stride;
+    for(unsigned int m = 1; m < matrix_count; m++)
+    {
+        T const * const matrix = matrix_list[m];
+        T* kronmat_new = new T[size_kron*matrix_size];
+        kronecker_product(kronmat, size_kron, stride_kron, matrix, matrix_size, matrix_stride, kronmat_new);
+        if(m-1 > 0) delete[] kronmat; // do not delete input matrix
+        kronmat = kronmat_new;
+        size_kron = size_kron*matrix_size;
+        stride_kron = size_kron;
+    }
+    // does the matrix vector product and adds the result to output
+    matrix_vector_product(kronmat, size_kron, intput, output);
+    delete[] kronmat;
+}
+
+/*
+ * naive implementation of batched kronmult
+ */
+template<typename T>
+void kronmult_batched_naive(const int matrix_count, const int matrix_size, T const * const matrix_list_batched[], const int matrix_stride,
+                      T* input_batched[],
+                      T* output_batched[], T* workspace_batched[],
+                      const int nb_batch)
+{
+    for(int i=0; i < nb_batch; i++)
+    {
+        T const * const * matrix_list = &matrix_list_batched[i*matrix_count];
+        T* input = input_batched[i];
+        T* output = output_batched[i];
+        T* workspace = workspace_batched[i];
+        kronmult_naive(matrix_count, matrix_size, matrix_list, matrix_stride, input, output, workspace);
+    }
+}
--- tests/utils/utils_cpu.h
+++ tests/utils/utils_cpu.h
@@ -3,7 +3,7 @@
 #include "data_generation.h"
 
 /*
- * wraps a batch of raw arrays with a proper constructor and destructor
+ * wraps a batch of arrays on device with a proper constructor and destructor
  */
 template<typename T>
 class ArrayBatch
@@ -11,39 +11,39 @@ class ArrayBatch
   public:
     // lets you access the device pointer directly
     T** rawPointer;
-    // host vector full of pointers to device arrays
-    std::vector<T*> batchRawPointers;
+    size_t nb_arrays;
 
-    // creates an array of `nb_arrays` arrays of size `array_sizes`
-    ArrayBatch(const size_t array_sizes, const size_t nb_arrays, const bool should_initialize_data=false): batchRawPointers(nb_arrays)
+    // creates an array of `nb_arrays` arrays of size `array_sizes` on device
+    ArrayBatch(const size_t array_sizes, const size_t nb_arrays_arg, const bool should_initialize_data=false): nb_arrays(nb_arrays_arg)
     {
         // random number generator for the data generation
         std::random_device rd{};
         std::default_random_engine rng{rd()};
-        // allocates all the batch elements
-        #pragma omp parallel for
+        // allocating the arrays
+        rawPointer = new T*[nb_arrays];
         for(unsigned int i=0; i<nb_arrays; i++)
         {
-            batchRawPointers[i] = new T[array_sizes];
-            if(should_initialize_data) fillArray(batchRawPointers[i], nb_arrays, rng);
+            rawPointer[i] = new T[array_sizes];
+            if(should_initialize_data) fillArray(rawPointer[i], nb_arrays, rng);
         }
-        // pointer to the batch elements
-        rawPointer = batchRawPointers.data();
     }
 
     // releases the memory
     ~ArrayBatch()
     {
-        for(auto ptr: batchRawPointers)
+        // frees the batch elements
+        for(unsigned int i=0; i<nb_arrays; i++)
         {
-            delete[] ptr;
+            delete[] rawPointer[i];
         }
+        // free the array of batch elements
+        delete[] rawPointer;
     }
 };
 
 /*
  * wraps a batch of arrays on device with a proper constructor and destructor
- * make sure that the batch contains only a given number of distinct elements
+ * make sure that the batch onctains only a given number of distinct elements
  */
 template<typename T>
 class ArrayBatch_withRepetition
@@ -51,40 +51,39 @@ class ArrayBatch_withRepetition
   public:
     // lets you access the device pointer directly
     T** rawPointer;
-    // host vector full of pointers
-    std::vector<T*> batchRawPointers;
-    std::vector<T*> batchRawPointers_withRepetition;
+    size_t nb_arrays_distinct;
 
-    // creates an array of `nb_arrays` arrays of size `array_sizes`
-    // contains only `nb_arrays_distinct` distinct elements
-    ArrayBatch_withRepetition(const size_t array_sizes, const size_t nb_arrays, const size_t nb_arrays_distinct=5, const bool should_initialize_data=false): batchRawPointers(nb_arrays_distinct), batchRawPointers_withRepetition(nb_arrays)
+    // creates an array of `nb_arrays` arrays of size `array_sizes` on device
+    // contains only `nb_arrays_distinct` distinct elemnts
+    ArrayBatch_withRepetition(const size_t array_sizes, const size_t nb_arrays, const size_t nb_arrays_distinct_arg=5, const bool should_initialize_data=false): nb_arrays_distinct(nb_arrays_distinct_arg)
     {
         // random number generator for the data generation
         std::random_device rd{};
         std::default_random_engine rng{rd()};
-        // allocates all the batch elements on device
+        // allocating the arrays
+        rawPointer = new T*[nb_arrays];
         for(unsigned int i=0; i<nb_arrays_distinct; i++)
         {
-            batchRawPointers[i] = new T[array_sizes];
-            if(should_initialize_data) fillArray(batchRawPointers[i], nb_arrays, rng);
+            rawPointer[i] = new T[array_sizes];
+            if(should_initialize_data) fillArray(rawPointer[i], nb_arrays, rng);
         }
         // allocates blocks of identical batch elements
-        #pragma omp parallel for
-        for(unsigned int i=0; i<nb_arrays; i++)
+        for(unsigned int i=nb_arrays_distinct; i<nb_arrays; i++)
         {
             const int ptr_index = (i * nb_arrays_distinct) / nb_arrays;
-            batchRawPointers_withRepetition[i] = batchRawPointers[ptr_index];
+            rawPointer[i] = rawPointer[ptr_index];
         }
-        // copy the pointers to batch elements on device
-        rawPointer = batchRawPointers_withRepetition.data();
     }
 
     // releases the memory
     ~ArrayBatch_withRepetition()
     {
-        for(auto ptr: batchRawPointers)
+        // frees the batch elements
+        for(unsigned int i=0; i<nb_arrays_distinct; i++)
         {
-            delete[] ptr;
+            delete[] rawPointer[i];
         }
+        // free the array of batch elements
+        delete[] rawPointer;
     }
 };
