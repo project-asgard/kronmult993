7697027f952222cdc0d963b5caa506bf97fdba68
Nestor Demeure <nestordemeure@gmail.com>
2021-07-30 10:11:53 -0700

    updated comments improved indentation

--- tests/kronmult_bench_gpu.cpp
+++ tests/kronmult_bench_gpu.cpp
@@ -21,9 +21,9 @@ long runBench(int const degree, int const dimension, int const grid_level, std::
     int const matrix_stride = 67; // large prime integer, modelize the fact that columns are not adjascent in memory
     int const batch_count = compute_batch_size(degree, dimension, grid_level, nb_distinct_outputs);
     std::cout << benchName << " benchcase"
-    << " batch_count:" << batch_count << " matrix_size:" << matrix_size
-    << " matrix_count:" << matrix_count << " size_input:" << size_input
-    << " nb_distinct_outputs:" << nb_distinct_outputs << std::endl;
+              << " batch_count:" << batch_count << " matrix_size:" << matrix_size
+              << " matrix_count:" << matrix_count << " size_input:" << size_input
+              << " nb_distinct_outputs:" << nb_distinct_outputs << std::endl;
 
     // allocates a problem
     // we do not put data in the vectors/matrices as it doesn't matter here
@@ -33,7 +33,7 @@ long runBench(int const degree, int const dimension, int const grid_level, std::
     DeviceArrayBatch<Number> workspace_batched(size_input, batch_count);
     DeviceArrayBatch_withRepetition<Number> output_batched(size_input, batch_count, nb_distinct_outputs);
 
-    // runs kronmult several times and displays the average runtime
+    // runs kronmult and displays the runtime
     std::cout << "Starting Kronmult" << std::endl;
     auto start                = std::chrono::high_resolution_clock::now();
     cudaError const errorCode = kronmult_batched(
@@ -62,7 +62,7 @@ int main()
     cudaError const errorCode = cudaGetDeviceProperties(&deviceProp, deviceId);
     checkCudaErrorCode(errorCode, "cudaSetDevice");
     std::cout << "GPU device:" << deviceId << " threadsAvailable:" << threadsPerBlock
-    << " architecture:" << deviceProp.major << '.' << deviceProp.minor << std::endl;
+              << " architecture:" << deviceProp.major << '.' << deviceProp.minor << std::endl;
 
     // running the benchmarks
     auto toy       = runBench(4, 1, 2, "toy");
@@ -73,10 +73,10 @@ int main()
 
     // display results
     std::cout << std::endl
-    << "Results:" << std::endl
-    << "toy: " << toy << "ms" << std::endl
-    << "small: " << small << "ms" << std::endl
-    << "medium: " << medium << "ms" << std::endl
-    << "large: " << large << "ms" << std::endl
-    << "realistic: " << realistic << "ms" << std::endl;
+              << "Results:" << std::endl
+              << "toy: " << toy << "ms" << std::endl
+              << "small: " << small << "ms" << std::endl
+              << "medium: " << medium << "ms" << std::endl
+              << "large: " << large << "ms" << std::endl
+              << "realistic: " << realistic << "ms" << std::endl;
 }
